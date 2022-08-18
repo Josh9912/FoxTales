2034a951058eceb74e8f6327a97dc436cb4c47d4
eyselljo <joshua.eysell@uni-a.de>
2022-08-18 09:56:22 +0200

    added basic template

---
+++ script_templates/project_templates.cs
@@ -0,0 +1,25 @@
+using System;
+using Godot;
+
+namespace Skripts
+{
+	public class %CLASS% : %BASE%
+	{
+		/// <summary>
+		/// Called when the node enters the sceen tree for the frist time. 
+		/// </summary>
+		public override void _Ready()
+		{
+			
+		}
+
+		/// <summary>
+		/// Called every frame
+		/// </summary>
+		/// <param name="delta"> is the elapsed time since the previous frame</param>
+		public override void _Process(float delta)
+		{
+
+		}
+	}
+}
