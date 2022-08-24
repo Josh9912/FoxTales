340abae948a1d810268b82320e97bbfeb52534e5
Josh9912 <68182082+Josh9912@users.noreply.github.com>
2022-08-24 20:16:32 +0200

    player

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,9 +4,36 @@ namespace FoxTales.Scripts
 {
 	public class Player : KinematicBody2D
 	{
+		[Export]
+		public int Speed = 100;
+
+		public Vector2 Velocity;
+
+		private const int Acceleration = 10;
+
+		public void GetInput()
+		{
+			Velocity = new Vector2();
+
+			if (Input.IsActionPressed("ui_right"))
+				Velocity.x += 1;
+			
+			if (Input.IsActionPressed("ui_left"))
+				Velocity.x -= 1;
+			
+			if (Input.IsActionPressed("ui_down"))
+				Velocity.y += 1;
+			
+			if (Input.IsActionPressed("ui_up"))
+				Velocity.y -= 1;
+
+			Velocity = Velocity.Normalized() * Speed;
+		}
+		
 		public override void _PhysicsProcess(float delta)
 		{
-			GD.Print("Hello Godo!");
+			GetInput();
+			Velocity = MoveAndSlide(Velocity * delta * Acceleration);
 		}
 	}
 }
